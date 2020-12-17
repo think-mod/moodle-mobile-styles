88358adf6829a2cdada280819d660cb49f7b2a89
Will Nahmens <willnahmens@gmail.com>
2020-12-17 11:51:42 +0100

    initial commit for mobile stylesheet

---
+++ lib.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @package local_mobile
+ * @author Will Nahmens
+ * @license http://www.gnu.org/copyleft/gpl.html
+ * @var staClass $plugin
+ */
---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package mobile
+ * @author Will Nahmens
+ * @license http://www.gnu.org/copyleft/gpl.html
+ * @var staClass $plugin
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_mobile';
+$plugin->version = 2820871988;
+$plugin->requires = 2016052300; //moodle version
+
